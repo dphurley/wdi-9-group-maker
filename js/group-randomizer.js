170a6f17a2a64d95978ca7673096fd2b24926f86
Danny Hurley <hurley.dannyp@gmail.com>
2017-03-13 08:52:29 -0400

    Determining each group number size. Just need to mark students as unavailable once they've been used now.

--- js/group-randomizer.js
+++ js/group-randomizer.js
@@ -1,5 +1,43 @@
 $(function () {
 
+    // CONFIG
+    var students = [
+        { id: 1, name: "Alex Gadd", available: true },
+        { id: 2, name: "Aylin McGinnis", available: true },
+        { id: 3, name: "Blake Smith", available: true },
+        { id: 4, name: "Brandon King", available: true },
+        { id: 5, name: "Jibril Sulaiman II", available: true },
+        { id: 6, name: "Charles Wilkinson", available: true },
+        { id: 7, name: "Dan Milo", available: true },
+        { id: 8, name: "David Hernquist", available: true },
+        { id: 9, name: "Dee Davis", available: true },
+        { id: 10, name: "Erin Martin", available: true },
+        { id: 11, name: "Felicia Garcia", available: true },
+        { id: 12, name: "Harry Karambizi", available: true },
+        { id: 13, name: "Hassan Masroor", available: true },
+        { id: 14, name: "Jacob Butler", available: true },
+        { id: 15, name: "Josh Seipel", available: true },
+        { id: 16, name: "Joy Sutton", available: true },
+        { id: 17, name: "Julian Ireland Chandler", available: true },
+        { id: 18, name: "Lanchana Shivananda", available: true },
+        { id: 19, name: "Liam Robinson", available: true },
+        { id: 20, name: "Maggie Kinney", available: true },
+        { id: 21, name: "Matt Atkins", available: true },
+        { id: 22, name: "Michael Torres", available: true },
+        { id: 23, name: "Neil Morris", available: true },
+        { id: 24, name: "Peter Colella", available: true },
+        { id: 25, name: "Randy Galeano", available: true },
+        { id: 26, name: "Ray Meibaum", available: true },
+        { id: 27, name: "Rush Myers", available: true },
+        { id: 28, name: "Sungmin Ro", available: true }
+    ];
+    randomizeStudentList();
+
+    var numberOfGroups = 5;
+    var numberPerGroup = Math.floor(students.length / numberOfGroups);
+    var remainderStudents = students.length % numberOfGroups;
+    // END CONFIG
+
     var groupNumber = 0;
     displayGroupNumber();
 
@@ -35,52 +73,39 @@ $(function () {
         students.sort(function(){return 0.5 - Math.random()});
     }
 
-    // function determineStudentsToDisplay () {
-    //     var numberOfGroups = students.sort
-    // }
+    function determineStudentsToDisplay () {
+        var studentsToDisplay = [];
+
+        var availableStudents = students.filter(function (student) {
+            return student.available === true;
+        });
+
+        var addOneOrZero = remainderStudents > 0 ? 1 : 0;
+        var numberOfStudentsForThisGroup = numberPerGroup + addOneOrZero;
+
+        for(var i = 0; i < numberOfStudentsForThisGroup; i++) {
+            studentsToDisplay.push(availableStudents[i]);
+        }
+
+        decrementRemainderStudents();
+        return studentsToDisplay;
+    }
+
+    function decrementRemainderStudents () {
+        if(remainderStudents > 0) remainderStudents--;
+    }
 
     $("#next-group-button").on("click", function () {
 
-        // var studentsToDisplay = determineStudentsToDisplay();
-
-        var studentNamesDOMElement = buildStudentNamesDOMElement(students);
-        displayStudentNames(studentNamesDOMElement);
-
         updateGroupNumber();
         displayGroupNumber();
+
+        var studentsToDisplay = determineStudentsToDisplay();
+        var studentNamesDOMElement = buildStudentNamesDOMElement(studentsToDisplay);
+        displayStudentNames(studentNamesDOMElement);
+
+        //then mark students that have been displayed as unavailable
+
     });
-
-    var students = [
-        { id: 1, name: "Alex Gadd", available: true },
-        { id: 2, name: "Aylin McGinnis", available: true },
-        { id: 3, name: "Blake Smith", available: true },
-        { id: 4, name: "Brandon King", available: true },
-        { id: 5, name: "Jibril Sulaiman II", available: true },
-        { id: 6, name: "Charles Wilkinson", available: true },
-        { id: 7, name: "Dan Milo", available: true },
-        { id: 8, name: "David Hernquist", available: true },
-        { id: 9, name: "Dee Davis", available: true },
-        { id: 10, name: "Erin Martin", available: true },
-        { id: 11, name: "Felicia Garcia", available: true },
-        { id: 12, name: "Harry Karambizi", available: true },
-        { id: 13, name: "Hassan Masroor", available: true },
-        { id: 14, name: "Jacob Butler", available: true },
-        { id: 15, name: "Josh Seipel", available: true },
-        { id: 16, name: "Joy Sutton", available: true },
-        { id: 17, name: "Julian Ireland Chandler", available: true },
-        { id: 18, name: "Lanchana Shivananda", available: true },
-        { id: 19, name: "Liam Robinson", available: true },
-        { id: 20, name: "Maggie Kinney", available: true },
-        { id: 21, name: "Matt Atkins", available: true },
-        { id: 22, name: "Michael Torres", available: true },
-        { id: 23, name: "Neil Morris", available: true },
-        { id: 24, name: "Peter Colella", available: true },
-        { id: 25, name: "Randy Galeano", available: true },
-        { id: 26, name: "Ray Meibaum", available: true },
-        { id: 27, name: "Rush Myers", available: true },
-        { id: 28, name: "Sungmin Ro", available: true }
-    ];
-
-    randomizeStudentList();
 
 });
