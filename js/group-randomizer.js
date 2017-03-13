c984b8314425baeac8f399daf40fe4fda64ae438
Danny Hurley <hurley.dannyp@gmail.com>
2017-03-13 08:39:49 -0400

    Updating Group Number logic and giving each student an ID.

--- js/group-randomizer.js
+++ js/group-randomizer.js
@@ -1,10 +1,12 @@
 $(function () {
 
-    var groupNumber = 1;
+    var groupNumber = 0;
     displayGroupNumber();
 
     function displayGroupNumber () {
-        $("#group-id").html(groupNumber);
+        if(groupNumber > 0) {
+            $("#group-id").html(groupNumber);
+        }
     }
 
     function updateGroupNumber () {
@@ -34,13 +36,10 @@ $(function () {
     }
 
     // function determineStudentsToDisplay () {
-    //
+    //     var numberOfGroups = students.sort
     // }
 
     $("#next-group-button").on("click", function () {
-
-        randomizeStudentList();
-        console.log(students);
 
         // var studentsToDisplay = determineStudentsToDisplay();
 
@@ -52,34 +51,36 @@ $(function () {
     });
 
     var students = [
-        { name: "Alex Gadd" },
-        { name: "Aylin McGinnis" },
-        { name: "Blake Smith" },
-        { name: "Brandon King" },
-        { name: "Jibril Sulaiman II" },
-        { name: "Charles Wilkinson" },
-        { name: "Dan Milo" },
-        { name: "David Hernquist" },
-        { name: "Dee Davis" },
-        { name: "Erin Martin" },
-        { name: "Felicia Garcia" },
-        { name: "Harry Karambizi" },
-        { name: "Hassan Masroor" },
-        { name: "Jacob Butler" },
-        { name: "Josh Seipel" },
-        { name: "Joy Sutton" },
-        { name: "Julian Ireland Chandler" },
-        { name: "Lanchana Shivananda" },
-        { name: "Liam Robinson" },
-        { name: "Maggie Kinney" },
-        { name: "Matt Atkins" },
-        { name: "Michael Torres" },
-        { name: "Neil Morris" },
-        { name: "Peter Colella" },
-        { name: "Randy Galeano" },
-        { name: "Ray Meibaum" },
-        { name: "Rush Myers" },
-        { name: "Sungmin Ro" }
+        { id: 1, name: "Alex Gadd", available: true },
+        { id: 2, name: "Aylin McGinnis", available: true },
+        { id: 3, name: "Blake Smith", available: true },
+        { id: 4, name: "Brandon King", available: true },
+        { id: 5, name: "Jibril Sulaiman II", available: true },
+        { id: 6, name: "Charles Wilkinson", available: true },
+        { id: 7, name: "Dan Milo", available: true },
+        { id: 8, name: "David Hernquist", available: true },
+        { id: 9, name: "Dee Davis", available: true },
+        { id: 10, name: "Erin Martin", available: true },
+        { id: 11, name: "Felicia Garcia", available: true },
+        { id: 12, name: "Harry Karambizi", available: true },
+        { id: 13, name: "Hassan Masroor", available: true },
+        { id: 14, name: "Jacob Butler", available: true },
+        { id: 15, name: "Josh Seipel", available: true },
+        { id: 16, name: "Joy Sutton", available: true },
+        { id: 17, name: "Julian Ireland Chandler", available: true },
+        { id: 18, name: "Lanchana Shivananda", available: true },
+        { id: 19, name: "Liam Robinson", available: true },
+        { id: 20, name: "Maggie Kinney", available: true },
+        { id: 21, name: "Matt Atkins", available: true },
+        { id: 22, name: "Michael Torres", available: true },
+        { id: 23, name: "Neil Morris", available: true },
+        { id: 24, name: "Peter Colella", available: true },
+        { id: 25, name: "Randy Galeano", available: true },
+        { id: 26, name: "Ray Meibaum", available: true },
+        { id: 27, name: "Rush Myers", available: true },
+        { id: 28, name: "Sungmin Ro", available: true }
     ];
+
+    randomizeStudentList();
 
 });
